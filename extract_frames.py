db0fb777615c10e8516cfac8f678adb257ec55a9
Praneeth Chandra Bogineni <pranchan@scc2.bu.edu>
2021-04-22 07:40:00 -0400

    merge commit

--- extract_frames.py
+++ extract_frames.py
@@ -7,4 +7,4 @@ save_path = './data/syn-weiz/weizman/'
 
 for video in paths:
     file_name = video.split('/')[-1][:-4]
-    sp.run(['ffmpeg', '-i', video, '-r', '1', f'{save_path}{file_name}_%04d.png'])
+    sp.run(['ffmpeg', '-i', video, f'{save_path}{file_name}_%04d.png'])
