93236b87235f4a68f15e9d827fabf9ea5649fa2c
praneethchandraa <praneeth.bogineni@gmail.com>
2021-04-27 03:03:06 +0530

    64x64

--- syn_weiz_data.py
+++ syn_weiz_data.py
@@ -8,39 +8,40 @@ import subprocess as sp
 
 def synthPrep(pth):
     img = cv2.imread(pth)
-    img = cv2.resize(img, (128, 384), interpolation = cv2.INTER_AREA)
-    board = 255*np.ones((384, 384, 3), dtype=np.uint8)
-    board[:, :128, :] = img
+    img = cv2.resize(img, (64, 192), interpolation = cv2.INTER_AREA)
+    board = 255*np.ones((192, 192, 3), dtype=np.uint8)
+    board[:, :64, :] = img
     return board
 
 def makeSynGrid(pths, out):
-    for i in range(0, 1000, 10):
+    for i in range(0, 10, 10):
         imgs = [cv2.imread(pths[k]) for k in range(i, i+10)]
-        imgs = [cv2.resize(i, (128, 384), interpolation = cv2.INTER_AREA) for i in imgs]
-        grid = 255*np.ones((1280, 1280, 3), dtype=np.uint8)
+        imgs = [cv2.resize(i, (64, 192), interpolation = cv2.INTER_AREA) for i in imgs]
+        grid = 255*np.ones((640, 640, 3), dtype=np.uint8)
 
         for j in range(10):
-            grid[128*j:128*(j+1), 0:128, :] = imgs[j][128:256, 0:128, :]
+            grid[64*j:64*(j+1), 0:64, :] = imgs[j][64:128, 0:64, :]
             
         cv2.imwrite(out+f"image_{i}.png", grid)
 
-pths_r = glob('./data/syn-weiz/weizman/*png')
-pths_syn = glob('./data/syn-weiz/synaction/*.png')
+pths_r = glob('./data/sample/weizman_c/*png')
+pths_syn = glob('./data/sample/synaction/*.png')
 
 shuffle(pths_r), shuffle(pths_syn)
 
-out = './data/syn_weiz/'
+out = './data/syn_weiz_6/'
 
 sp.run(['mkdir', '-p', out+'real_/real'])
 
-for pth in pths_r[:-10]:
+for pth in pths_r[:-6]:
     file = out+'real_/real/'+pth.split('/')[-1]
     img = cv2.imread(pth)
+
     cv2.imwrite(file, img)
 
 sp.run(['mkdir', '-p', out+'synth_/synth'])
 
-for pth in pths_syn[:-1000]:
+for pth in pths_syn[:-10]:
     file = out+'synth_/synth/'+pth.split('/')[-1]
     img = synthPrep(pth)
     cv2.imwrite(file, img)
@@ -48,7 +49,7 @@ for pth in pths_syn[:-1000]:
 sp.run(['mkdir', '-p', out+'rows_'])
 sp.run(['mkdir', '-p', out+'rows_/real'])
 
-for pth in pths_r[-10:]:
+for pth in pths_r[-6:]:
     file = out+'rows_/real/'+pth.split('/')[-1]
     img = cv2.imread(pth)
     cv2.imwrite(file, img)
@@ -56,4 +57,4 @@ for pth in pths_r[-10:]:
 
 sp.run(['mkdir', '-p', out+'rows_/synth'])
 
-makeSynGrid(pths_syn[-1000:], out+'rows_/synth/')
+makeSynGrid(pths_syn[-10:], out+'rows_/synth/')
